23d7ca552fc66d0e2e50b099300c3dcc7aac95a8
gunasekaran2410 <61088656+gunasekaran2410@users.noreply.github.com>
2021-03-18 16:11:24 +0530

    change old file

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
 import Head from "next/head"
 import { Component } from 'react'
 import { attributes, react as HomeContent } from '../content/home.md';
-import Navigation from '../components/Nav';
-import styles from './Index.module.css'
-import Slide from "../components/Slide";
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 
 export default class Home extends Component {
   render() {
@@ -16,45 +10,18 @@ export default class Home extends Component {
         <Head>
           <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         </Head>
-        <Navigation/>
-        <Slide/>
-    
-
-    {/* <article>
-          <h1 >{title}</h1>
+        <article>
+          <h1>{title}</h1>
           <HomeContent />
           <ul>
             {cats.map((cat, k) => (
               <li key={k}>
-             
+                <h2>{cat.name}</h2>
+                <p>{cat.description}</p>
               </li>
             ))}
           </ul>
-        </article> */}
-    <section>
-       
-       <div className="text-center">
-<h3 className={styles.red} className=" text-uppercase">{title}
-</h3>
-       </div>
-       <Container>
-       <HomeContent />
-       <Row>
-       {cats.map((cat, k) => (
-    <Col xs={6} md={4} key={k}>
-       <h2>{cat.name}</h2>
-                <p>{cat.description}</p>
-    </Col>
-  
-      ))}
-  </Row>
-  
-</Container>
-     </section>
-   
-
-
-
+        </article>
       </>
     )
   }
